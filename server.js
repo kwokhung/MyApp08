a64c0e0b1aa4c8182e9ed5be229e3cca38e15fc7
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-07-03 16:33:33 +0800

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 8080);
 
 function handler(req, res) {
     fs.readFile(__dirname + '/index.html',
